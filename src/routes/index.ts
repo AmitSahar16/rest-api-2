d0fec864376ebd55663cb6bdba7eb965ffbf00cd
sivan <sivanalc@gmail.com>
2026-01-28 00:58:33 +0200

    add auth routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import authRoutes from './auth_route';
+import userRoutes from './users_route';
 import postRoutes from './posts_route';
 import commentRoutes from './comments_route';
 
 const router = express.Router();
 
+router.use('/auth', authRoutes);
+router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 
